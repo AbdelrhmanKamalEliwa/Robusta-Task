e1b20617ee42b6f5fbef257f9d2bcb4f21094592
Abdelrhman Eliwa <abdelrhmankamaleliwa@gmail.com>
2022-11-14 21:18:53 +0200

    Adding Domain Layer

---
+++ Robusta-Task/Scenes/Repos/Domain/Entities/ReposResponse.swift
@@ -0,0 +1,38 @@
+//
+//  ReposResponse.swift
+//  Repositories
+//
+//  Created by Abdelrhman Eliwa on 09/11/2022.
+//
+
+import Foundation
+
+//struct ReposResponse: Decodable {
+//    let date: Date?
+//}
+
+struct ReposResponse: Decodable, Identifiable {
+    let id: Int?
+    let name: String?
+    let fullName: String?
+    let isPrivate: Bool?
+    let owner: Owner?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case fullName = "full_name"
+        case isPrivate = "private"
+        case owner
+    }
+}
+
+struct Owner: Decodable {
+    let id: Int?
+    let avatarURL: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case avatarURL = "avatar_url"
+    }
+}
